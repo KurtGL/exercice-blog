d85944879e3aa2bb397ee9931744aead5a95cf7a
Cyrille <Cyrille@MacBook-Pro-de-Cyrille.local>
2018-12-20 22:28:36 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
   title = 'exercice-blogxxxx';
 
   ListeDesPosts: Post[] = [
-    { "title": "Bonjour", "content": "Je m'appelle Marcel et c'est mon premier message. Bisous.", "loveIts": -1, "created_at":new Date("04/30/1980") }, 
-    { "title": "Suite", "content": "Tout va très bien, merci.", "loveIts": 1, "created_at":new Date("04/30/1980") }
+    { "title": "Bonjour", "content": "Je m'appelle Marcel et c'est mon premier message. Bisous.", "loveIts": -1, "created_at":new Date("04/30/1980 20:50") }, 
+    { "title": "Suite", "content": "Tout va très bien, merci.", "loveIts": 1, "created_at":new Date("04/30/1980 18:25") }
   ];
 
   constructor() {
